c6ab46472d37e0a2a18551e46371d4f98c2d7ae8
odo <odo@mac.com>
2013-02-02 17:37:32 +0100

    using new appendix server

--- src/poe_appendix_sup.erl
+++ src/poe_appendix_sup.erl
@@ -4,13 +4,13 @@
 
 -define(SERVER, ?MODULE).
 
--export([start_link/0, start_child/2, init/1]).
+-export([start_link/0, start_child/3, init/1]).
 
 start_link() ->
 	supervisor:start_link({local, ?SERVER}, ?MODULE, []).
 
-start_child(Topic, Dir) ->
-	supervisor:start_child(?SERVER, [Dir, Topic, [{trap_exit, true}]]).
+start_child(Topic, Dir, Timeout) ->
+	supervisor:start_child(?SERVER, [Dir, Topic, Timeout]).
 
 init([]) ->
 	AppendixServer = {appendix_server, {appendix_server, start_link_with_id, []}, temporary, 5000, worker, [appendix_server, bisect]},
