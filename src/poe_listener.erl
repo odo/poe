cf045202e191c6a8cf93395fcaac82fdbbd847a6
odo <odo@mac.com>
2013-02-18 13:29:42 +0100

    returning emty message list on non-existent topic

--- src/poe_listener.erl
+++ src/poe_listener.erl
@@ -14,20 +14,25 @@ init(ListenerPid, Socket, Transport, _Opts = []) ->
 loop(Socket, ranch_tcp) ->
 	case poe_protocol:read_command(Socket) of
 		{pointer_request, Topic, Pointer, Limit} ->
-				Server = poe_server:read_pid(Topic, Pointer),
 				Header1 = poe_protocol:add_message_type(poe_protocol:message_type(data_reply), <<>>),
-				case appendix_server:file_pointer(Server, Pointer, Limit) of
-					{FileName, Position, LengthData} ->
-						Length = LengthData + byte_size(Header1),
-						Header2 = poe_protocol:add_length(Length, Header1),
-						gen_tcp:send(Socket, Header2),
-						{ok, File} = file:open(FileName, [raw, binary]),
-						{ok, LengthData} = file:sendfile(File, Socket, Position, LengthData, []),
-						file:close(File);
+				case poe_server:read_pid(Topic, Pointer) of
 					not_found ->
-						Length = byte_size(Header1),
-						Header2 = poe_protocol:add_length(Length, Header1),
-						gen_tcp:send(Socket, Header2)
+						Header2 = poe_protocol:add_length(0, Header1),
+						gen_tcp:send(Socket, Header2);
+					Server ->
+						case appendix_server:file_pointer(Server, Pointer, Limit) of
+							{FileName, Position, LengthData} ->
+								Length = LengthData + byte_size(Header1),
+								Header2 = poe_protocol:add_length(Length, Header1),
+								gen_tcp:send(Socket, Header2),
+								{ok, File} = file:open(FileName, [raw, binary]),
+								{ok, LengthData} = file:sendfile(File, Socket, Position, LengthData, []),
+								file:close(File);
+							not_found ->
+								Length = byte_size(Header1),
+								Header2 = poe_protocol:add_length(Length, Header1),
+								gen_tcp:send(Socket, Header2)
+						end
 				end,
 				loop(Socket, ranch_tcp);
 		{put_request, Topic, Messages} ->
